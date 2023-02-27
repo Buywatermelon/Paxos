1bca49a07be3ba78b18250eaa956a8fd8fb85165
Buywatermelon <1505127742@qq.com>
2023-02-27 20:51:26 +0800

    Changes

--- paxos.go
+++ paxos.go
@@ -1,32 +1,60 @@
 package main
 
 import (
-	"fmt"
-	"log"
-	"net/http"
-	"os"
+	"testing"
 )
 
-func main() {
-	http.HandleFunc("/", indexHandler)
-	port := os.Getenv("PORT")
-	if port == "" {
-		port = "8080"
-		log.Printf("Defaulting to port %s", port)
+func start(acceptorIds []int, learnerIds []int) ([]*Acceptor, []*Learner) {
+	acceptors := make([]*Acceptor, 0)
+	for _, aid := range acceptorIds {
+		a := newAcceptor(aid, learnerIds)
+		acceptors = append(acceptors, a)
 	}
 
-	log.Printf("Listening on port %s", port)
-	log.Printf("Open http://localhost:%s in the browser", port)
-	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%s", port), nil))
+	learners := make([]*Learner, 0)
+	for _, lid := range learnerIds {
+		l := newLearner(lid, acceptorIds)
+		learners = append(learners, l)
+	}
+
+	return acceptors, learners
 }
 
-func indexHandler(w http.ResponseWriter, r *http.Request) {
-	if r.URL.Path != "/" {
-		http.NotFound(w, r)
-		return
+func cleanup(acceptors []*Acceptor, learners []*Learner) {
+	for _, a := range acceptors {
+		a.close()
 	}
-	_, err := fmt.Fprint(w, "Hello, World!")
-	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
+
+	for _, l := range learners {
+		l.close()
 	}
+}
+
+func TestSingleProposer(t *testing.T) {
+	// 1001、1002、1003是接受者id
+	acceptorIds := []int{1001, 1002, 1003}
+	// 2001是学习者id
+	learnerIds := []int{2001}
+	acceptors, learners := start(acceptorIds, learnerIds)
+
+	defer cleanup(acceptors, learners)
+
+	p := &Proposer{
+		id:        1,
+		acceptors: acceptorIds,
+	}
+
+	value := p.propose("hello world")
+	if value != "hello world" {
+		t.Errorf("value = %s, excepted %s", value, "helo world")
+	}
+
+	learnValue := learners[0].chosen()
+	if learnValue != value {
+		t.Errorf("learnValue = %s, exceptd %s", learnValue, "hello world")
+	}
+}
+
+func TestTwoProposers(t *testing.T) {
+
 }
